517defc3638ab47b77fb1c01832d33c68c7b1daa
Gregory Rehm <grehm87@gmail.com>
2018-08-09 23:09:26 -0700

    more changes to data transfer

--- transfer_data.py
+++ transfer_data.py
@@ -10,6 +10,30 @@ SERVER_DIRNAME = '/x1/data/results/backups/'
 DATA_PATH = 'data/experiment1/training/raw'
 
 
+def get_first_days_data(patient_id, initial_dt):
+    hour_glob = "{{{}}}".format(",".join(["{0:02d}".format(i) for i in range(initial_dt.hour - 2, 24)]))
+    start_glob = "*{}-{}-{}-{}-*".format(initial_dt.year, initial_dt.month, initial_dt.day, hour_glob)
+
+    one_day_later = initial_dt + timedelta(days=1)
+    hour_glob = "{{{}}}".format(",".join(["{0:02d}".format(i) for i in range(0, one_day_later.hour+1)]))
+    end_glob = "*{}-{}-{}-{}-*".format(one_day_later.year, one_day_later.month, one_day_later.day, hour_glob)
+
+    patient_start_path = os.path.join(SERVER_DIRNAME, patient_id, start_glob)
+    proc = subprocess.Popen(
+        ['scp', '{}:{}'.format(SERVER_NAME, patient_start_path), DATA_PATH],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    proc.communicate()
+    patient_start_path = os.path.join(SERVER_DIRNAME, patient_id, end_glob)
+    proc = subprocess.Popen(
+        ['scp', '{}:{}'.format(SERVER_NAME, patient_start_path), DATA_PATH],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    proc.communicate()
+
+
 def copy_ards_patient(row):
     patient_id = row['Patient Unique Identifier']
     berlin_start = row['Date when Berlin criteria first met (m/dd/yyy)']
@@ -20,19 +44,7 @@ def copy_ards_patient(row):
         return
     # should also be grabbing data from at least 2 hours prior because of the way our file rotation
     # is set up
-    hour_glob = "{{{}}}".format(",".join(["{0:02d}".format(i) for i in range(dt.hour - 2, 24)]))
-    start_glob = "*{}-{}-{}-{}-*".format(dt.year, dt.month, dt.day, hour_glob)
-
-    one_day_later = dt + timedelta(days=1)
-    hour_glob = "{{{}}}".format(",".join(["{0:02d}".format(i) for i in range(0, one_day_later.hour+1)]))
-    end_glob = "*{}-{}-{}-{}-*".format(dt.year, dt.month, dt.day, hour_glob)
-
-    patient_start_path = os.path.join(SERVER_DIRNAME, patient_id, start_glob)
-    subprocess.Popen(
-        ['scp', '{}:{}'.format(SERVER_NAME, patient_start_path), DATA_PATH],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
+    get_first_days_data(patient_id, dt)
 
 
 def copy_non_ards_patient(row):
@@ -40,7 +52,11 @@ def copy_non_ards_patient(row):
     # now it might be best to just gather first 24 hrs of data. But in future
     # we will need to gather first 24 exclusively
     patient_id = row['Patient Unique Identifier']
-    pass
+    first_file_cmd = "ls {} | head -n 1".format(os.path.join(SERVER_DIRNAME, patient_id))
+    proc = subprocess.Popen(['ssh', SERVER_NAME, first_file_cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    init_dt = datatime.strptime(stdout, '%Y-%m-%d-%H-%M-%S.%f')
+    get_first_days_data(patient_id, init_dt)
 
 
 def main():
