1662e577ed512e6ca5a5d2bb9fd99d0b25fed61d
Greg Rehm <grehm87@gmail.com>
2018-08-09 19:58:04 -0700

    remove old and useless files from 2016

--- run_spark.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export SPARK_HOME=/home/ec2-user/spark
-export PYTHONPATH=$SPARK_HOME/python/:$PYTHONPATH
-export PYSPARK_PYTHON=/home/ec2-user/ecs251-final-project/venv/bin/python
-
-connect_str="spark://`lsof | grep TCP | grep 7077 | head -n1 | awk '{print $9}'`"
-source venv/bin/activate
-python learn.py --with-spark --connect-str $connect_str
