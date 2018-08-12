8e848980ccbfb0d9a926c3df4cdc6e54ebeced15
Gregory Rehm <grehm87@gmail.com>
2018-08-12 16:56:18 -0700

    add multiprocessing so we can run things in parallel in grid search

--- feature_grid_search.py
+++ feature_grid_search.py
@@ -1,5 +1,6 @@
 import argparse
 from itertools import compress, product
+from multiprocessing import Pool
 import os
 
 import pandas as pd
@@ -22,14 +23,48 @@ def get_all_possible_features():
         # XXX Add pressure itime eventually, altho it may only be useful for PC/PS pts.
         ('dyn_compliance', 39), ('TVratio', 11)
     ]
+    # There is actually no way that we can do grid search on all possible broad features
+    # because it will yield 8388608 possibilities, which is infeasible to search thru.
+    # So I will only look through the ones that seem reasonable from my POV. Reducing #
+    # features down to 17 possibilities still yields 262144 choices. 13 possibilities gives
+    # 16384 choices
     all_possibilities = all_possible_flow_time_features + [
-        ('TVi', 9), ('TVe', 10), ('Maw', 16), ('ipAUC', 18), ('PIP', 15), ('PEEP', 17),
-        ('epAUC', 19), ('maxF', 12), ('minF', 13), ('maxP', 14), ('min_pressure', 35),
-        ('vol_at_05', 40), ('vol_at_76', 41), ('vol_at_1', 42),
+        ('TVi', 9), ('PIP', 15), ('PEEP', 17), ('vol_at_76', 41), ('min_pressure', 35)
     ]
     all_ft_combos = (set(compress(all_possible_flow_time_features, mask)) for mask in product(*[[0,1]]*len(all_possible_flow_time_features)))
     all_combos = (set(compress(all_possibilities, mask)) for mask in product(*[[0,1]]*len(all_possibilities)))
     return {'flow_time_gen': all_ft_combos, 'broad_gen': all_combos}
+
+
+
+def run_model(model_args, combo, model_idx, possible_folds, out_dir):
+    results = {folds: {'auc': 0} for folds in possible_folds}
+    results['idx'] = model_idx
+    if not combo:
+        results['features'] = []
+        return results
+    features = [k[0] for k in combo]
+    results['features'] = features
+
+    path = os.path.join(out_dir, 'dataset-{}.pkl'.format(model_idx))
+    if os.path.exists(path):
+        dataset = pd.read_pickle(path)
+    else:
+        dataset = Dataset(model_args.cohort_description, 'custom', model_args.stacks, True, custom_features=combo).get()
+        dataset.to_pickle(path)
+
+    for folds in possible_folds:
+        model_args.folds = folds
+        model = ARDSDetectionModel(model_args, dataset)
+        model.train_and_test()
+        model_auc = roc_auc_score(model.results.patho.tolist(), model.results.prediction.tolist())
+        results[folds]['auc'] = model_auc
+        del model  # paranoia
+    return results
+
+
+def func_star(args):
+    return run_model(*args)
 
 
 def main():
@@ -48,29 +83,16 @@ def main():
     feature_combos = get_all_possible_features()
     possible_folds = [5, 10]
     out_dir = DF_DIR.format(experiment_num=main_args.experiment, feature_set=main_args.feature_set)
-    for i, combo in enumerate(feature_combos['{}_gen'.format(main_args.feature_set)]):
-        if not combo:
-            continue
-        features = [k[0] for k in combo]
+    feature_gen = feature_combos['{}_gen'.format(main_args.feature_set)]
 
-        path = os.path.join(out_dir, 'dataset-{}.pkl'.format(i))
-        if os.path.exists(path):
-            dataset = pd.read_pickle(path)
-        else:
-            dataset = Dataset(model_args.cohort_description, 'custom', model_args.stacks, True, custom_features=combo).get()
-            dataset.to_pickle(path)
+    input_gen = [(model_args, combo, idx, possible_folds, out_dir) for idx, combo in enumerate(feature_gen)]
 
-        results[i] = {i: dict() for i in possible_folds}
-        for folds in possible_folds:
-            model_args.folds = folds
-            model = ARDSDetectionModel(model_args, dataset)
-            model.train_and_test()
-            model_auc = roc_auc_score(model.results.patho.tolist(), model.results.prediction.tolist())
-            results[i][folds] = {'features': features, 'auc': model_auc}
-            del model  # paranoia
-        del dataset  # paranoia
+    pool = Pool(10)
+    results = pool.map(func_star, input_gen)
+    pool.close()
+    pool.join()
 
-    best = max([(i, results[i][folds]['auc']) for i in results for folds in possible_folds], key=lambda x: x[1])
+    best = max([(features_run['idx'], features_run[folds]['auc']) for features_run in results for folds in possible_folds], key=lambda x: x[1])
     print('Best AUC: {}'.format(best[1]))
     print('Best features: {}'.format(results[best[0]]))
     dict_ = pickle.dumps(results)
