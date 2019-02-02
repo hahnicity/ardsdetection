cf882edf44167630575560c554687e4fbb8f9ac7
Greg Rehm <grehm87@gmail.com>
2019-02-02 01:05:51 -0800

    default for all experiments should be 1+4

--- patient_time_categorization.py
+++ patient_time_categorization.py
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('-p', '--from-pickle')
     parser.add_argument('-t', '--to-pickle')
     parser.add_argument('-d', '--cohort-description', default='cohort-description.csv', help='Path to file describing the cohort')
-    parser.add_argument('-e', '--experiment', default='1')
+    parser.add_argument('-e', '--experiment', default='1+4')
     args = parser.parse_args()
 
     if args.from_pickle:
