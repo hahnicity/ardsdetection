d34615ff72ecf721eee981dd50d9510e4a765325
Greg Rehm <grehm87@gmail.com>
2019-03-07 21:16:04 -0800

    remove more duplicate patients from experiment 4 and fix non phasic coverage script so that we can find coverage for ARDS cohort

--- non_phasic_coverage_plots.py
+++ non_phasic_coverage_plots.py
@@ -13,14 +13,26 @@ from cohort_tools.quality_check import find_hourly_coverage
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('phase_file')
+    parser.add_argument('--cohort-file', default='cohort-description.csv')
     parser.add_argument('data_frame')
     args = parser.parse_args()
 
     df = pd.read_pickle(args.data_frame)
     df = df.rename(columns={'abs_time_at_BS': 'abs_bs'})
     df['breath_time'] = df['iTime'] + df['eTime']
-    phases = pd.read_csv(args.phase_file)
+    cohort = pd.read_csv(args.cohort_file)
+    cohort = cohort[((cohort.experiment_group == 1) | (cohort.experiment_group == 4)) & (cohort['Potential Enrollment'] == 'Y')]
+    # make phases file from cohort file
+    ards_patients = df[df.y == 1].patient.unique()
+    other_patients = df[(df.y == 0) | (df.y == 2)].patient.unique()
+
+    ards_vent_starts = cohort.loc[cohort['Patient Unique Identifier'].isin(ards_patients), ['Patient Unique Identifier', 'Date when Berlin criteria first met (m/dd/yyy)']]
+    ards_vent_starts = ards_vent_starts.rename(columns={'Date when Berlin criteria first met (m/dd/yyy)': 'vent_start_time'})
+    other_vent_starts = cohort.loc[cohort['Patient Unique Identifier'].isin(other_patients), ['Patient Unique Identifier', 'vent_start_time']]
+
+    phases = pd.concat([ards_vent_starts, other_vent_starts])
+    phases = phases.rename(columns={'vent_start_time': 'vent_start', 'Patient Unique Identifier': 'patient'})
+    phases['vent_end'] = pd.to_datetime(phases.vent_start) + pd.Timedelta(hours=24)
 
     hour_idxs = perform_patient_hour_mapping(df, phases, 'no')
     coverage = find_hourly_coverage(df, hour_idxs)
